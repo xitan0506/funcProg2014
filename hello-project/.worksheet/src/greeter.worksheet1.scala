de8389ca660cb3366d2a88d79455a6f85e7b7e95
xitan0506 <xitan0506@gmail.com>
2014-04-26 07:51:47 -0400

    Scala hello object and worksheet1
    Signed-off-by: xitan0506 <xitan0506@gmail.com>

---
+++ hello-project/.worksheet/src/greeter.worksheet1.scala
@@ -0,0 +1,12 @@
+package greeter
+
+object worksheet1 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(80); 
+  println("Welcome to the Scala worksheet");$skip(12); 
+  val x = 1;System.out.println("""x  : Int = """ + $show(x ));$skip(47); 
+  def increase(x : Int) : Int = {
+  	x + 1
+  };System.out.println("""increase: (x: Int)Int""");$skip(14); val res$0 = 
+  increase(x);System.out.println("""res0: Int = """ + $show(res$0));$skip(22); 
+  val y = increase(x);System.out.println("""y  : Int = """ + $show(y ))}
+  
+}
---
+++ hello-project/src/greeter/worksheet1.sc
@@ -0,0 +1,12 @@
+package greeter
+
+object worksheet1 {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  val x = 1                                       //> x  : Int = 1
+  def increase(x : Int) : Int = {
+  	x + 1
+  }                                               //> increase: (x: Int)Int
+  increase(x)                                     //> res0: Int = 2
+  val y = increase(x)                             //> y  : Int = 2
+  
+}
